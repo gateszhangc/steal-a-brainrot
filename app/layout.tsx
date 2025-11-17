df42bd087e3b28135f8474a5ed227958104fe601
gateszhangc <gateszhang92@gmail.com>
2025-11-17 13:54:56 +0800

    添加google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,6 +40,21 @@ export default function RootLayout({ children }: { children: ReactNode }) {
               '(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window, document, "clarity", "script", "u7dep5lbt5");'
           }}
         />
+        <Script
+          id="ga-gtag"
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-CP1SM9BK8E"
+        />
+        <Script
+          id="ga-gtag-config"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-CP1SM9BK8E');`
+          }}
+        />
       </head>
       <body className={`${nunito.variable} bg-night text-white`}>{children}</body>
     </html>
